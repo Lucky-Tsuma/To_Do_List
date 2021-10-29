9d256bd2d9f42e8c69b3db8c2bd208731e69cf19
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-10-29 12:20:14 +0300

    Display tasks to when they are added. (map)

--- src/components/TasksArea.js
+++ src/components/TasksArea.js
@@ -1,11 +1,28 @@
 import '../App.css';
+import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { removeToDo } from "../redux/actions/counterToDos";
+import { markComplete } from "../redux/actions/markCompleteAction";
 
-function TasksArea () {
+function Tasks () {
+    const  todos  = useSelector((state) => state.toDo);
+    const dispatch = useDispatch();
+
     return (
-        <div className="tasks_display_area">
-        <p>Your Task List</p>
-       </div>
+            <div className="tasks_display_area">
+                <p className="tasks_area_heading">Your Task List</p>
+                
+                {todos.map((task) => (
+                <div className="div_tasks">
+                        <p className="task_name" key={task.id} style={{ textDecoration: task.is_complete ? "line-through" : "" }}>{task.title}</p>
+                        <div className="div_buttons">
+                        <button className="btn_remove_task" onClick={() => dispatch(removeToDo(task.id))}>Remove Task</button>
+                        <button className="btn_mark_complete" onClick={() => dispatch(markComplete(task.id))}>Mark Complete</button>
+                        </div>
+                </div>
+                ))}
+            </div>
     );
 }
     
-export default TasksArea
+export default Tasks
