5ed3c28fe564c3157c51942837b76ca2c98ce586
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-11-01 13:15:48 +0300

    Rename file

--- src/redux/actions/counterToDos.js
+++ src/redux/actions/counterToDos.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, REMOVE_TODO } from "../types";
+import { ADD_TODO, REMOVE_TODO, MARK_COMPLETE } from "../types";
 
 export const addToDo = (todo) => {
     return {
@@ -10,6 +10,13 @@ export const addToDo = (todo) => {
 export const removeToDo = (id) => {
     return {
         type: REMOVE_TODO,
+        payload: id
+    }
+}
+
+export const markComplete = (id) => {
+    return {
+        type: MARK_COMPLETE,
         payload: id
     }
 }
